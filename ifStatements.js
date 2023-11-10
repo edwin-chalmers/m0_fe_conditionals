a2bb292f2af00a6b48db30e9e3f8668ea62c54e7
Edwin Chalmers <edwinchalmers@gmail.com>
2023-11-10 01:31:43 -0700

    Complete the assignment for ifStatements

--- ifStatements.js
+++ ifStatements.js
@@ -8,11 +8,13 @@
 // Write a conditional statement that prints only one or the other.
 // Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
 
-var numQuarters = 0;
+var numQuarters = 5;
 
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
-
+if (numQuarters >= 2) {
+    console.log("I have enough money for a gumball");
+} else {
+    console.log("I don't have enough money for a gumball");
+}
 
 // CHALLENGE 2
 // Using the variables defined below, write code that will tell you if you have the ingredients to make a pizza. 
@@ -26,6 +28,12 @@ console.log("I don't have enough money for a gumball");
 
 // Experiment with manipulating the value held within both variables to make sure all above conditions output what you expect.
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 5;
 var hasSauce = true;
+
+if (cupsOfFlour >= 2 && hasSauce === true) {
+    console.log("I can make a pizza!");
+} else {
+    console.log("I cannot make a pizza!");
+}
 
